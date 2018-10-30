58566d1718242268260747ab1c081684733ad58b
Scott Hain <shain@chef.io>
2018-10-30 14:21:42 -0700

    Update new build style
    Signed-off-by: Scott Hain <shain@chef.io>

--- .buildkite/scripts/build_windows_release.ps1
+++ .buildkite/scripts/build_windows_release.ps1
@@ -3,14 +3,19 @@
 #Requires -Version 5
 Set-PSDebug -Trace 1
 
+$Env:HAB_BLDR_CHANNEL="unstable"
+
+Write-Host "--- Install latest habitat binary"
+Invoke-Expression "choco install habitat --confirm" -ErrorAction Stop
 
 Write-Host "--- Moving build folder to new location"
 New-Item -ItemType directory -Path C:\build
 Copy-Item -Path C:\workdir\* -Destination C:\build -Recurse
 
 Write-Host "--- Running build"
-cd C:\build
-dir
+cd C:\build\components\hab
+Invoke-Expression "hab pkg build ." -ErrorAction Stop
+
 # Invoke-Expression "cargo build --release " -ErrorAction Stop
 
 # Write-Host "--- Uploading artifacts"
